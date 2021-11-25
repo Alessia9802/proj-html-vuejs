f1769fd2e5b2561ed0efb98239e1fc156f385b36
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-25 12:46:42 +0100

    Menu dropdown doesn't work properly

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,28 +8,37 @@
         alt=""
       />
       <!-- menu_nav -->
+      <div class="dropdown">
+        <div id="myDropdown" class="dropdown-content show">
+          <ul>
+            <li>
+              <a href="#"> {{ first_voice }} </a>
+            </li>
+            <li>
+              <a href="#"> {{ second_voice }} </a>
+            </li>
+            <li>
+              <a href="#">{{ third_voice }}</a>
+            </li>
+            <li>
+              <a href="#">{{ fourth_voice }}</a>
+            </li>
+            <li>
+              <a href="#">{{ fifth_voice }}</a>
+            </li>
+            <li>
+              <a href="#">{{ sixth_voice }}</a>
+            </li>
+          </ul>
+        </div>
 
-      <ul>
-        <li>
-          <a href="#"> {{ first_voice }} </a>
-        </li>
-        <li>
-          <a href="#"> {{ second_voice }} </a>
-        </li>
-        <li>
-          <a href="#">{{ third_voice }}</a>
-        </li>
-        <li>
-          <a href="#">{{ fourth_voice }}</a>
-        </li>
-        <li>
-          <a href="#">{{ fifth_voice }}</a>
-        </li>
-        <li>
-          <a href="#">{{ sixth_voice }}</a>
-        </li>
-      </ul>
-      <img class="menu_icon" src="../assets/img/menu_hamburger.png" alt="" />
+        <img
+          @click="myFunction()"
+          class="menu_icon dropbtn"
+          src="../assets/img/menu_hamburger.png"
+          alt=""
+        />
+      </div>
     </div>
   </div>
   <!-- site_header -->
@@ -78,5 +87,38 @@ ul > li {
     text-decoration: none;
     color: white;
   }
+}
+
+//Build menu open on click
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f1f1f1;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {
+  background-color: #ddd;
+}
+
+.show {
+  display: block;
 }
 </style>
