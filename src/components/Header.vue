a592ca3fa29f1baa3ef88b28b77fa24b3d8620f6
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-25 11:43:24 +0100

    Prepare menu nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="site_header">
-    <img src="../assets/img/avada-music-logo.png" alt="" />
+    <div class="top_nav">
+      <!-- logo -->
+      <img
+        class="logo"
+        src="../assets/img/avada-music-logo-retina.png"
+        alt=""
+      />
+      <!-- menu_nav -->
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Meet the Band</a></li>
+        <li><a href="#">Live Dates</a></li>
+        <li><a href="#">Latest News</a></li>
+        <li><a href="#">Albums</a></li>
+        <li><a href="#">Fans</a></li>
+      </ul>
+      <img class="menu_icon" src="../assets/img/menu_hamburger.png" alt="" />
+    </div>
   </div>
   <!-- site_header -->
 </template>
@@ -9,4 +27,34 @@
 export default {};
 </script>
 
-<style></style>
+<style lang="scss">
+#site_header {
+  background-size: cover;
+  background-image: url("../assets/img/home_slider.jpg");
+  height: 100vh;
+}
+
+.top_nav {
+  justify-content: space-between;
+  display: flex;
+}
+
+.top_nav > .logo {
+  width: 400px;
+  padding: 1.5rem;
+}
+
+.top_nav .menu_icon {
+  width: 40px;
+  height: 40px;
+  filter: invert(100%);
+}
+
+ul > li {
+  list-style: none;
+  a {
+    text-decoration: none;
+    color: white;
+  }
+}
+</style>
