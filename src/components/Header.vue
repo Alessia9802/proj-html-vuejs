cb54d6bb476075453268a92d1b4c0cf3e5b674f1
Alessia9802 <alessiamisciagna@yahoo.it>
2021-11-25 11:12:48 +0100

    Base Layout structure vue.js

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="site_header">
+    <img src="../assets/img/avada-music-logo.png" alt="" />
+  </div>
+  <!-- site_header -->
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
